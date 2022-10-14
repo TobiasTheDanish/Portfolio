2b45e880aedaa36828c5e3f8b614ef0958795258
TobiasTheDanish <t.hc199811@gmail.com>
2022-10-14 10:32:46 +0200

    Add project files.

---
+++ Models/ProjectModel.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorWasmTest.Models
+{
+    public class ProjectModel
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string[]? Tags { get; set; }
+
+        public ProjectModel(string? name, string? description, string[]? tags)
+        {
+            Name = name;
+            if (description != null && description.Length > 50)
+            {
+                Description = description.Substring(0, 50).TrimEnd() + "...";
+            }
+            else
+            {
+                Description = description;
+            }
+
+            Tags = tags;
+        }
+    }
+}
