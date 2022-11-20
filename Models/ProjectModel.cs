21f1af69d98470d6dda22a006b9617aa65d202da
TobiasTheDanish <t.hc199811@gmail.com>
2022-11-20 17:29:21 +0100

    Changes to data and view of project

--- Models/ProjectModel.cs
+++ Models/ProjectModel.cs
@@ -6,12 +6,12 @@
         public string? Description { get; set; }
         public string? ShortDescription { get; set; }
         public string[]? Tags { get; set; }
+        public string[]? Images { get; set; }
 
-        public ProjectModel(string? name, string? description, string[]? tags)
+        public ProjectModel(string? name, string? description, string[]? tags, string[]? images)
         {
             Name = name;
             Description = description;
-
             if (description != null && description.Length > 50)
             {
                 ShortDescription = description.Substring(0, 50).TrimEnd() + "...";
@@ -22,6 +22,7 @@
             }
 
             Tags = tags;
+            Images = images;
         }
     }
 }
